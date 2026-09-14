3db0ca887e37cf20a646201a4cc2d24aa9ea9654
Stéphane Bélien <stephane.belien@rte-france.com>
2026-09-14 22:38:42 +0200

    Remplacement des classes par des functions compatibles PIC Suppression de la gestion mémoire dynamiqe (HEAP) mal supportée par les PIC

---
+++ src/DYPlayer_PIC.c
@@ -0,0 +1,406 @@
+/**
+ * Abstraction of basic features of the DY-SV17F mp3 player board, written for
+ * Arduino, should work on other frameworks as well. Instead of DY-SV17F I will
+ * from here on refer to it as the "module".
+ *
+ * There are some virtual methods that MUST be overridden (serialRead and
+ * serialWrite) and one that you may override (begin)
+ */
+//#include <ctype.h>
+#include <string.h>
+#include <ctype.h>
+#include "DYPlayer_PIC.h"
+
+
+#ifdef __cplusplus
+namespace DY
+{
+#endif
+
+//  private prototype
+
+
+
+
+
+
+void DYPlayer_init(dy_player_t *player, dy_uart_write_fn_t uart_write_fn, dy_uart_read_fn_t uart_read_fn, uint8_t options)
+{
+    player->uart_write = uart_write_fn;
+    player->uart_read = uart_read_fn;
+    player->options = options;
+}
+
+/**
+  * Calculate the sum of all bytes in a buffer as a simple "CRC".
+  * @param data pointer to bytes to calculate the CRC for.
+  * @param size of buffer.
+  * @return Checksum of the buffer.
+  */
+static uint8_t  checksum(const uint8_t *data, uint8_t size)
+{
+  uint8_t sum = 0;
+  for (uint8_t i = 0; i < size; i++)
+  {
+    sum = sum + data[i];
+  }
+  return sum;
+}
+
+/**
+  * Validate data buffer with CRC byte (last byte should be the CRC byte).
+  * @param data pointer to bytes to calculate the CRC for.
+  * @param size of data.
+  * @return boolean indicating CRC is correct (true) or incorrect (false).
+  */
+static bool  validateCrc(uint8_t *data, uint8_t size)
+{
+  uint8_t crc = data[size - 1];
+  return checksum(data, size - 1) == crc;
+}
+
+/**
+  * Send a command to the module, adds a CRC to the passed buffer.
+  * @param dy_player pointer to the player instance.
+  * @param command The command to send to the module.
+  * @param data pointer to bytes to send to the module (can be NULL if no data).
+  * @param size of data (0 if no data).
+  */
+static bool sendCommand(dy_player_t *player,uint8_t command, uint8_t *data, uint8_t size)
+{
+  if (player == NULL || player->uart_write == NULL) return false;
+
+  uint8_t buffer[3];
+  buffer[0] = 0xAA;
+  buffer[1] = command;
+  buffer[2] = size;
+  player->uart_write(buffer, sizeof(buffer));
+
+  uint8_t crc[1]={0};
+  crc[0] = checksum(buffer, sizeof(buffer));
+  if (data != NULL && size > 0) {
+    player->uart_write(data, size);
+    crc[0] += checksum(data, size);
+  }
+
+  player->uart_write(crc, 1);
+
+  return true;
+}
+  
+/**
+  * Send command with converted paths to  weird format required by the
+  * modules.
+  *
+  * - Any dot in a path should become a star (`*`)
+  * - Path ending slashes should be have a star prefix, except root.
+  *
+  * E.g.: /SONGS1/FILE1.MP3 should become: /SONGS1﹡/FILE1*MP3
+  * NOTE: This comment uses a unicode * look-a-alike (﹡) because ﹡/ end the
+  * comment.
+  * @param player pointer to the player instance.
+  * @param command The command to send.
+  * @param device A [DY::Device member](#typedef-enum-class-dydevice_t),
+  *               e.g  `DY::Device::Flash` or `DY::Device::Sd`.
+  * @param path of the file (asbsolute).
+  */
+static bool byPathCommand(dy_player_t *player, uint8_t command, device_t device, const char *path)
+{
+  if (player == NULL || player->uart_write == NULL || path == NULL) return false;
+
+  size_t size_size = strlen(path);
+  if (size_size == 0 || size_size > UINT8_MAX) {
+    return false;
+  }
+
+  uint8_t size = (uint8_t)size_size;
+
+  uint8_t transformed_len = size;
+  for (uint8_t i = 1; i < size; i++) {
+    if (path[i] == '/') {
+      transformed_len++;
+    }
+  }
+
+  if (transformed_len > DY_MAX_PATH_LEN) {
+      return false;
+  } 
+
+  uint8_t header[5];
+  header[0] = 0xAA;
+  header[1] = command;
+  header[2] = transformed_len + 1; // + device byte
+  header[3] = (uint8_t)device;
+  header[4] = toupper((unsigned char)path[0]);
+
+  uint8_t crc = checksum(header, sizeof(header));
+
+  player->uart_write(header, sizeof(header));
+
+  for (uint8_t i = 1; i < size; i++) {
+    char c = path[i];
+
+    if (c == '.') {
+      uint8_t byte = '*';
+      crc += byte;
+      player->uart_write(&byte, 1);
+    } else if (c == '/') {
+      uint8_t bytes[2] = { '*', '/' };
+      crc += bytes[0] + bytes[1];
+      player->uart_write(bytes, sizeof(bytes));
+    } else {
+      uint8_t byte = (uint8_t)toupper((unsigned char)c);
+      crc += byte;
+      player->uart_write(&byte, 1);
+    }
+  }
+
+  player->uart_write(&crc, 1);
+  return true;
+}
+
+/** Public API */
+
+  play_state_t DYPlayer_checkPlayState(dy_player_t *player)
+  {
+    if (player == NULL || player->uart_read == NULL)
+        return PLAY_STATE_FAIL;
+
+
+    sendCommand(player, 0x01, NULL, 0);
+    uint8_t buffer[5];
+    if (player->uart_read(buffer, 5))
+    {
+      return (play_state_t)buffer[3];
+    }
+    return PLAY_STATE_FAIL;
+  }
+
+  void DYPlayer_play(dy_player_t *player)
+  {
+    sendCommand(player, 0x02, NULL, 0);
+  }
+
+  void DYPlayer_pause(dy_player_t *player)
+  {
+    sendCommand(player, 0x03, NULL, 0);
+  }
+
+  void DYPlayer_stop(dy_player_t *player)
+  {
+    sendCommand(player, 0x04, NULL, 0);
+  }
+
+  void DYPlayer_previous(dy_player_t *player)
+  {
+    sendCommand(player, 0x05, NULL, 0);
+  }
+
+  void DYPlayer_next(dy_player_t *player)
+  {
+    sendCommand(player, 0x06, NULL, 0);
+  }
+
+  void DYPlayer_playSpecified(dy_player_t *player, uint16_t number)
+  {
+    uint8_t data[2] = {
+        (uint8_t)(number >> 8),
+        (uint8_t)(number & 0xff)
+    };
+
+    sendCommand(player, 0x07, data, 2);
+  }
+  
+  void DYPlayer_playSpecifiedDevicePath(dy_player_t *player, device_t device,const char *path)
+  {
+    byPathCommand(player, 0x08, device, path);
+  }
+
+  device_t DYPlayer_getPlayingDevice(dy_player_t *player)
+  {
+    if (!sendCommand(player, 0x0a, NULL, 0))
+        return DEVICE_FAIL;
+
+    uint8_t buffer[5];
+    if (player->uart_read(buffer, 5))
+    {
+      return (device_t)buffer[3];
+    }
+    return DEVICE_FAIL;
+  }
+
+  void DYPlayer_setPlayingDevice(dy_player_t *player, device_t device)
+  {
+    uint8_t data[1] = {(uint8_t)device};
+    sendCommand(player, 0x0b, data, 1);  }
+
+  uint16_t DYPlayer_getSoundCount(dy_player_t *player)
+  {
+    if (!sendCommand(player, 0x0c, NULL, 0))
+        return 0;
+    
+    uint8_t buffer[6];
+    if (player->uart_read(buffer, 6))
+    {
+      return (buffer[3] << 8) | buffer[4];
+    }
+    return 0;
+  }
+
+  uint16_t DYPlayer_getPlayingSound(dy_player_t *player)
+  {
+    if (!sendCommand(player, 0x0d, NULL, 0))
+        return 0;
+
+    uint8_t buffer[6];
+    if (player->uart_read(buffer, 6))
+    {
+      return (buffer[3] << 8) | buffer[4];
+    }
+    return 0;
+  }
+
+  void DYPlayer_previousDir(dy_player_t *player, playDirSound_t song)
+  {
+    if (song == PREVIOUS_DIR_LAST_SOUND)
+    {
+      sendCommand(player, 0x0e, NULL, 0);
+    }
+    else
+    {
+      sendCommand(player, 0x0f, NULL, 0);
+    }
+  }
+
+  uint16_t DYPlayer_getFirstInDir(dy_player_t *player)
+  {
+    if (!sendCommand(player, 0x11, NULL, 0))
+        return 0;
+      
+    uint8_t buffer[6];
+    if (player->uart_read(buffer, 6))
+    {
+      return (buffer[3] << 8) | buffer[4];
+    }
+    return 0;
+  }
+
+  uint16_t DYPlayer_getSoundCountDir(dy_player_t *player)
+  {
+    if (!sendCommand(player, 0x12, NULL, 0))
+        return 0;
+
+    uint8_t buffer[6];
+    if (player->uart_read(buffer, 6))
+    {
+      return (buffer[3] << 8) | buffer[4];
+    }
+    return 0;
+  }
+
+  void DYPlayer_setVolume(dy_player_t *player, uint8_t volume)
+  {
+    uint8_t data[1] = {volume};
+    sendCommand(player, 0x13, data, 1);
+  }
+
+  void DYPlayer_volumeIncrease(dy_player_t *player)
+  {
+    sendCommand(player, 0x14, NULL, 0);
+  }
+
+  void DYPlayer_volumeDecrease(dy_player_t *player)
+  {
+    sendCommand(player, 0x15, NULL, 0);
+  }
+
+  void DYPlayer_interludeSpecified(dy_player_t *player, device_t device, uint16_t number)
+  {
+    uint8_t data[3] = {0};
+    data[0] = (uint8_t)device;
+    data[1] = number >> 8;
+    data[2] = number & 0xff;
+    sendCommand(player, 0x16, data, 3);
+  }
+
+  void DYPlayer_interludeSpecifiedDevicePath(dy_player_t *player, device_t device, const char *path)
+  {
+    byPathCommand(player, 0x17, device, path);
+  }
+
+  void DYPlayer_stopInterlude(dy_player_t *player)
+  {
+    sendCommand(player, 0x10, NULL, 0);
+  }
+
+  void DYPlayer_setCycleMode(dy_player_t *player, play_mode_t mode)
+  {
+    uint8_t data[1] = {mode};
+    sendCommand(player, 0x18, data, 1);
+  }
+
+  void DYPlayer_setCycleTimes(dy_player_t *player, uint16_t cycles)
+  {
+    uint8_t data[2] = {0};
+    data[0] = cycles >> 8;
+    data[1] = cycles & 0xff;
+    sendCommand(player, 0x19, data, 2);
+  }
+
+  void DYPlayer_setEq(dy_player_t *player, eq_t eq)
+  {
+    uint8_t data[1] = { (uint8_t)eq };
+    sendCommand(player, 0x1a, data, 1);
+  }
+
+  void DYPlayer_select(dy_player_t *player, uint16_t number)
+  {
+    uint8_t data[2] = {0};
+    data[0] = number >> 8;
+    data[1] = number & 0xff;
+    sendCommand(player, 0x1f, data, 2);
+  }
+
+  void DYPlayer_combinationPlay(dy_player_t *player, const char *sounds[], uint8_t size)
+  {
+  if (sounds == NULL || size == 0) {
+      return;
+  }
+
+  for (uint8_t i = 0; i < size; i++) {
+      if (sounds[i] == NULL ||
+          sounds[i][0] == '\0' ||
+          sounds[i][1] == '\0') {
+          return;
+      }
+  }
+
+    // This part of the command can be easily determined already.
+    uint8_t header[3] = {0xaa, 0x1b, size*2};
+
+    // Depends on the length, checksum is a sum so we can add the other values
+    // later.
+    uint8_t crc = checksum(header, 3);
+
+    // Send the command and length already.
+    player->uart_write(header , 3);
+
+    // Send each pair of chars containing the file name and add the values of
+    // each char to the crc.
+    for (uint8_t i = 0; i < size; i++)
+    {
+      crc += checksum((const uint8_t *)sounds[i], 2);
+      player->uart_write((const uint8_t *)sounds[i], 2);
+    }
+    // Lastly, write the crc value.
+    player->uart_write(&crc, 1);
+  }
+
+  void DYPlayer_endCombinationPlay(dy_player_t *player)
+  {
+    sendCommand(player, 0x1c, NULL, 0);
+  }
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/DYPlayer_PIC.h
@@ -0,0 +1,389 @@
+/**
+ * Abstraction of basic features of the DY-SV17F mp3 player board, written for
+ * Arduino, should work on other frameworks as well. Instead of DY-SV17F I will
+ * from here on refer to it as the "module".
+ *
+ * There are some virtual methods that MUST be overridden (serialRead and
+ * serialWrite) and one that you may override (begin)
+ */
+#ifndef DYPLAYER_PIC_H
+#define DYPLAYER_PIC_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//#define DY_PATH_LEN 20
+#define DY_MAX_PATH_LEN 36
+
+#ifdef __cplusplus
+namespace DY
+{
+#endif
+
+/**
+  * Options for the DYPlayer module.
+  * These options can be combined using bitwise OR.
+  * For example, to enable both AUX_IN and ONLY_FLASH options, you would use:
+  * `options = OPTION_AUX_IN | OPTION_ONLY_FLASH;`
+  *
+  * OPTION_AUX_IN     - The canal AUX input is present on the module.
+  * OPTION_ONLY_FLASH - Only the flash storage is available on the module.
+  */
+typedef uint8_t dy_option_t;
+enum
+{
+    OPTION_AUX_IN     = 0x01,
+    OPTION_ONLY_FLASH = 0x02
+};
+
+
+/**
+  * Storage devices reported by module and to choose from when selecting a
+  * storage device.
+  */
+typedef uint8_t device_t;
+
+enum
+{
+    DEVICE_USB       = 0x00,
+    DEVICE_SD        = 0x01,
+    DEVICE_FLASH     = 0x02,
+    DEVICE_FAIL      = 0xfe,
+    DEVICE_NO_DEVICE = 0xff
+};
+
+/**
+  * The current module play state.
+  */
+typedef int8_t play_state_t;
+
+enum
+{
+    PLAY_STATE_FAIL    = -1,
+    PLAY_STATE_STOPPED = 0,
+    PLAY_STATE_PLAYING = 1,
+    PLAY_STATE_PAUSED  = 2
+};
+
+/**
+  * Equalize settings.
+  */
+typedef uint8_t eq_t;
+
+enum
+{
+    EQ_NORMAL = 0,
+    EQ_POP,
+    EQ_ROCK,
+    EQ_JAZZ,
+    EQ_CLASSIC
+};
+
+/**
+  * Play modes are basically whatever you commonly find on a media player,
+  * i.e.:
+  * Repeat 1, Repeat all, Repeat list (dir), playlist (by dir), random play.
+  *
+  * The default is perhaps somewhat unexpected: `DY::PlayMode::OneOff`. Often
+  * these modules will be used in toys or information displays where you can
+  * press a button and hear a corresponding sound. To get default media player
+  * behaviour, you should probably set `DY::PlayMode::Sequence` to just continue
+  * playing the next song until all are played or skipped, then stop.
+  */
+typedef uint8_t play_mode_t;
+
+enum
+{
+    PLAY_MODE_REPEAT = 0,
+    PLAY_MODE_REPEAT_ONE,
+    PLAY_MODE_ONE_OFF,
+    PLAY_MODE_RANDOM,
+    PLAY_MODE_REPEAT_DIR,
+    PLAY_MODE_RANDOM_DIR,
+    PLAY_MODE_SEQUENCE_DIR,
+    PLAY_MODE_SEQUENCE
+};
+
+
+/**
+  * The `DY::DYPlayer::previousDir()` method expects this type as its argument.
+  * Imagine you would press a button on a media player that selects the
+  * previous directory/playlist, do you expect it to play the first song of
+  * that list, or the last one? Depending on what you find logical or on your
+  * requirement, this enumeration allows you to choose what happens when you
+  * go to the previous directory.
+  */
+  
+  typedef uint8_t playDirSound_t;
+  enum
+  {
+      PREVIOUS_DIR_FIRST_SOUND = 0,
+      PREVIOUS_DIR_LAST_SOUND
+  };  
+
+  typedef void (*dy_uart_write_fn_t)(const uint8_t *buffer, uint8_t size);
+  typedef bool (*dy_uart_read_fn_t)(uint8_t *buffer, uint8_t size);
+
+  typedef struct
+  {
+    dy_uart_read_fn_t uart_read;
+    dy_uart_write_fn_t uart_write;
+    uint8_t options;
+  } dy_player_t;
+
+/**
+  * Initialize the DYPlayer module.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param uart_write_fn function pointer for UART write.
+  * @param uart_read_fn function pointer for UART read.
+  * @param opt_aux_in_used indicates if the AUX input is used.
+  * @param opt_only_flash indicates if only the flash storage is available.
+  */
+void DYPlayer_init(dy_player_t *player, dy_uart_write_fn_t uart_write_fn, dy_uart_read_fn_t uart_read_fn, uint8_t options);
+
+/**
+  * Check the current play state can, be called at any time.
+  * @param player pointer to the `dy_player_t` structure.
+  * @return Play status: A [`DY::PlayState`](#typedef-enum-class-dyplay_state_t),
+  *         e.g DY::PlayMode::Stopped, DY::PlayMode::Playing, etc.
+  */
+play_state_t DYPlayer_getPlayState(dy_player_t *player);
+
+/**
+  * Play the currently selected file from the start.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_play(dy_player_t *player);
+
+/**
+  * Set the play state to paused.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_pause(dy_player_t *player);
+
+/**
+  * Set the play state to stopped.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_stop(dy_player_t *player);
+
+/**
+  * Play the previous file.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_previous(dy_player_t *player);
+
+/**
+  * Play the next file.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_next(dy_player_t *player);
+
+/**
+  * Play a sound file by number, number sent as 2 bytes.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param number of the file, e.g. `1` for `00001.mp3`.
+  */
+void DYPlayer_playSpecified(dy_player_t *player, uint16_t number);
+
+/**
+  * Play a sound file by device and path.
+  * Path may consist of up to 2 nested directories of 8 bytes long and a
+  * file name of 8 bytes long excluding the extension of 4 bytes long.
+  * If your directory names are shorter you can use more nesting. Use no
+  * more than 36 bytes for your paths. If you require more, check the
+  * readme, chapter: Memory use.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param device A [`DY::Device member`](#typedef-enum-class-dydevice_t),
+  *               e.g  `DY::Device::Flash` or `DY::Device::Sd`.
+  * @param path pointer to the path of the file (asbsolute).
+  */
+void DYPlayer_playSpecifiedDevicePath(dy_player_t *player, device_t device,const char *path);
+
+/**
+  * Get the storage device that is currently used for playing sound files.
+  *
+  * @param player pointer to the `dy_player_t` structure.
+  * @return a [`DY::Device` member](#typedef-enum-class-dydevice_t),
+  *         e.g  `DY::Device::Flash` or `DY::Device::Sd`.
+  */
+device_t DYPlayer_getPlayingDevice(dy_player_t *player);
+
+/**
+  * Set the device number the module should use.
+  * Tries to set the device but no guarantee is given, use `getDevice()`
+  * to check the actual current storage device.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param device A [`DY::Device` member](#typedef-enum-class-dydevice_t),
+  *               e.g  `DY::Device::Flash` or `DY::Device::Sd`.
+  */
+void DYPlayer_setPlayingDevice(dy_player_t *player, device_t device);
+
+/**
+  * Get the amount of sound files on the current storage device.
+  * @param player pointer to the `dy_player_t` structure.
+  * @return number of sound files.
+  */
+uint16_t DYPlayer_getSoundCount(dy_player_t *player);
+
+/**
+  * Get the currently playing file by number.
+  * @param player pointer to the `dy_player_t` structure.
+  * @return number of the file currently playing.
+  */
+uint16_t DYPlayer_getPlayingSound(dy_player_t *player);
+
+/**
+  * Select previous directory and start playing the first or last song.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param song Play `DY::PreviousDir::FirstSound` or
+  *             DY::PreviousDir::LastSound
+  */
+void DYPlayer_previousDir(dy_player_t *player, playDirSound_t song);
+
+/**
+  * Get number of the first song in the currently selected directory.
+  * @param player pointer to the `dy_player_t` structure.
+  * @return number of the first song in the currently selected directory.
+  */
+uint16_t DYPlayer_getFirstInDir(dy_player_t *player);
+
+/**
+  * Get the amount of sound files in the currently selected directory.
+  * NOTE: Excluding files in sub directories.
+  * @param player pointer to the `dy_player_t` structure.
+  * @return number of sound files in currently selected directory.
+  */
+uint16_t DYPlayer_getSoundCountDir(dy_player_t *player);
+
+/**
+  * Set the playback volume between 0 and 30.
+  * Default volume if not set: 20.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param volume to set (0-30)
+  */
+void DYPlayer_setVolume(dy_player_t *player, uint8_t volume);
+
+/**
+  * Increase the volume.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_volumeIncrease(dy_player_t *player);
+
+/**
+  * Decrease the volume.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_volumeDecrease(dy_player_t *player);
+
+/**
+  * Play an interlude file by device and number, number sent as 2 bytes.
+  * Note from the manual: "Music interlude" only has level 1. Continuous
+  * interlude will cover the previous interlude (the interlude will be
+  * played immediately). When the interlude is finished, it will return to
+  * the first interlude breakpoint and continue to play.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param device A [`DY::Device member`](#typedef-enum-class-dydevice_t),
+  *               e.g  `DY::Device::Flash` or `DY::Device::Sd`.
+  * @param number of the file, e.g. `1` for `00001.mp3`.
+  */
+void DYPlayer_interludeSpecified(dy_player_t *player, device_t device, uint16_t number);
+
+/**
+  * Play an interlude by device and path.
+  * Note from the manual: "Music interlude" only has level 1. Continuous
+  * interlude will cover the previous interlude (the interlude will be
+  * played immediately). When the interlude is finished, it will return to
+  * the first interlude breakpoint and continue to play.
+  *
+  * Path may consist of up to 2 nested directories of 8 bytes long and a
+  * file name of 8 bytes long excluding the extension of 4 bytes long.
+  * If your directory names are shorter you can use more nesting. Use no
+  * more than 36 bytes for your paths. If you require more, check the
+  * readme, chapter: Memory use.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param device A [`DY::Device member`](#typedef-enum-class-dydevice_t),
+  *               e.g  `DY::Device::Flash` or `DY::Device::Sd`.
+  * @param path pointer to the path of the file (asbsolute).
+  */
+void DYPlayer_interludeSpecifiedDevicePath(dy_player_t *player, device_t device,const char *path);
+
+/**
+  * Stop the interlude and continue playing.
+  * Will also stop the current sound from playing if interlude is not
+  * active.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_stopInterlude(dy_player_t *player);
+
+/**
+  * Sets the cycle mode.
+  * See [`DY::play_state_t`](#typedef-enum-class-dyplay_state_t) for modes
+  * and meaning.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param mode The cycle mode to set.
+  */
+void DYPlayer_setCycleMode(dy_player_t *player, play_mode_t mode);
+
+/**
+  * Set how many cycles to play when in cycle modes 0, 1 or 4 (repeat
+  * modes).
+  * @param player pointer to the `dy_player_t` structure.
+  * @param cycles The cycle count for repeat modes.
+  */
+void DYPlayer_setCycleTimes(dy_player_t *player, uint16_t cycles);
+
+/**
+  * Set the equalizer setting.
+  * See [`DY::eq_t`](#typedef-enum-class-dyeq_t) for settings.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param eq The equalizer setting.
+  */
+void DYPlayer_setEq(dy_player_t *player, eq_t eq);
+
+/**
+  * Select a sound file without playing it.
+  * @param player pointer to the `dy_player_t` structure.
+  * @param number of the file, e.g. `1` for `00001.mp3`.
+  */
+void DYPlayer_select(dy_player_t *player, uint16_t number);
+
+/**
+  * Combination play allows you to make a playlist of multiple sound files.
+  *
+  * You could use this to combine numbers e.g.: "fourthy-two" where you
+  * have samples for "fourthy" and "two".
+  *
+  * This feature has a particularly curious parameters, you have to
+  * specify the sound files by name, they have to be named by 2 numbers
+  * and an extension, e.g.: `01.mp3` and specified by `01`. You should
+  * pass them as an array pointer. You need to put the files into a
+  * directory that can be called `DY`, `ZH or `XY`, you will have to check
+  * the manual that came with your module, or try all of them. There may
+  * well be more combinations! Also see
+  * [Loading sound files](#loading-sound-files).
+  *
+  * E.g.
+  * ```cpp
+  * const char * sounds[2][3] = { "01", "02" };
+  * DY::DYPlayer::combinationPlay(sounds, 2);
+  * ````
+  * @param player pointer to the `dy_player_t` structure.
+  * @param sounds An array of char[2] containing the names of sounds to
+  *        play in order.
+  * @param size The length of the passed array.
+  */
+void DYPlayer_combinationPlay(dy_player_t *player,const char *sounds[], uint8_t size);
+
+/**
+  * End combination play.
+  * @param player pointer to the `dy_player_t` structure.
+  */
+void DYPlayer_endCombinationPlay(dy_player_t *player);
+
+
+#ifdef __cplusplus
+  };}
+#endif
+
+#endif // DYPLAYER_PIC_H
